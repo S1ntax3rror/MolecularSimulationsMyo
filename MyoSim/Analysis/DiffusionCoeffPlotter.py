cc929f396db5abf97b36e6a3c01b8e326b8c28fc
Jiri Kaeser <jiri@nxu.ch>
2025-02-28 17:29:20 +0100

    added analysis tool for diff coeff

--- MyoSim/Analysis/DiffusionCoeffPlotter.py
+++ MyoSim/Analysis/DiffusionCoeffPlotter.py
@@ -1,13 +1,83 @@
 import os
 import numpy as np
+from statsmodels.stats.correlation_tools import corr_nearest
+
+
+def get_npz_files(active_directory):
+    """
+    Returns npz files at path given
+    :param active_directory:
+    :return:
+    """
+    files = os.listdir(active_directory)
+
+    npz_list = []
+
+    for file in files:
+        if ".npz" in file:
+            npz_list.append(file)
+
+    return npz_list
+
+
+def get_num_jumps_in_npz(file):
+    """
+    returns the number of pocket jumps that happen in a npz file
+    :param file:
+    :return:
+    """
+
+    data = np.load(file)
+    coordinates = data["coordinates"]
+
+    mean = np.mean(coordinates, axis=0)
+    normalized_coords = coordinates - mean
+
+    count = 0
+    max_norm = 0
+
+    for coord in normalized_coords:
+        print(coord, mean)
+        norm = np.linalg.norm(coord)
+
+        if norm > 2:
+            count += 1
+
+        if max_norm < norm:
+            max_norm = norm
+    print(max_norm)
+    return count
+
 
 live = False
 
 if live:
-    data_basepath = "/home/kaeserj/PycharmProjects/CurveFitMorse/Data/FrozenMyoglobinPDBs"
+    cwd = os.getcwd()
 else:
-    data_basepath = "/home/kaeserj/PycharmProjects/CurveFitMorse/Data/FrozenMyoglobinPDBs"
+    cwd = os.getcwd() + "/../SimulationSetupAndRun"
 
-co_or_no_CO = [""]
+sim_dirs = [cwd + "/WITH_CO_V2/", cwd + "/NO_CO_V2/"]
+temp_dirs = ["1K", "5K", "10K", "50K", "100K", "300K"]
+possible_sub_dirs = np.arange(1, 51).tolist()
 
-main_directories =
+for i in range(len(possible_sub_dirs)):
+    possible_sub_dirs[i] = str(possible_sub_dirs[i])
+
+for sim_dir in sim_dirs:
+    for temp_dir in temp_dirs:
+        sub_dirs = sorted(os.listdir(sim_dir + temp_dir))
+        print("processing directory " + sim_dir.replace(cwd, "") + "/" + temp_dir)
+
+        number_of_jumps = 0
+
+        for sub_dir in sub_dirs:
+
+            if sub_dir in possible_sub_dirs:
+                active_dir = sim_dir + temp_dir + "/" + sub_dir
+
+                npz_files = get_npz_files(active_dir)
+
+                for npz_file in npz_files:
+                    number_of_jumps += get_num_jumps_in_npz(active_dir + "/" + npz_file)
+
+        print(number_of_jumps)
